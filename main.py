92264ff71d8b7c4a88a815c0154636556f97527d
gurland <stas.gurland@gmail.com>
2023-04-26 20:42:39 +0300

    fix: make proper output format and get input cases from files

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
-from cities_graph import CityNode, CitiesGraph
-
+from cities_graph import CitiesGraph
+from utils import get_test_cases, make_case_results_output
 
 if __name__ == '__main__':
-    g = CitiesGraph.from_case_text("""3
-    France 1 4 4 6
-    Spain 3 1 6 3
-    Portugal 1 1 2 2""")
+    input_file_path = input("Enter path to your input file (press enter for default one): ")
+    cases = get_test_cases(input_file_path)
 
-    country_cities = {}
-    for city_id, city_node in g:
-        country_cities.setdefault(city_node.country, set()).add(city_id)
+    case_results: dict[int, dict[list, int]] = {}
 
-    print(country_cities)
+    for case_number, case in enumerate(cases):
+        case_graph = CitiesGraph.from_case_text(case)
+        if not case_graph:
+            break
 
-    completed_cities = set()
-    completed_countries = set()
+        country_cities = {}
+        for city_id, city_node in case_graph:
+            country_cities.setdefault(city_node.country, set()).add(city_id)
 
-    for i in range(10000):
-        for completed_country in completed_countries:
-            country_cities.pop(completed_country)
-        completed_countries.clear()
+        completed_cities = set()
+        completed_countries = set()
 
-        for city_id, city_node in g:
-            if city_id in completed_cities:
-                continue
+        current_day = 0
+        while country_cities:
+            for completed_country in completed_countries:
+                country_cities.pop(completed_country)
+            completed_countries.clear()
 
-            if city_node.is_complete():
-                completed_cities.add(city_id)
+            for city_id, city_node in case_graph:
+                if city_id in completed_cities:
+                    continue
 
-        for country_name, city_ids in country_cities.items():
-            if completed_cities == {11, 12, 21, 22}:
-                print("qwe")
-            if completed_cities.intersection(city_ids) == city_ids:
-                completed_countries.add(country_name)
-                print(f"Completed country {country_name} in {i} days")
-        g = g.get_graph_in_a_one_day()
+                if city_node.is_complete():
+                    completed_cities.add(city_id)
+
+            for country_name, city_ids in country_cities.items():
+                if completed_cities.intersection(city_ids) == city_ids:
+                    completed_countries.add(country_name)
+                    case_results.setdefault(case_number, {})[country_name] = current_day
+
+            current_day += 1
+
+            case_graph = case_graph.get_graph_in_a_one_day()
+
+    print(make_case_results_output(case_results))
